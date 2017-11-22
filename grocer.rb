e2524af4e678cebaea4c95c947a466e694b13630
Kenny Hom <kenny.hom27@gmail.com>
2017-11-22 04:01:26 +0000

    automatically backed up by learn

--- grocer.rb
+++ grocer.rb
@@ -2,13 +2,11 @@ def consolidate_cart(cart)
   hash = {}
   cart.each do |items|
     items.each do |name, stats|
-      if hash[name]
+        hash[name] ||= stats
+        hash[name][:count] ||= 0
         hash[name][:count] += 1
-      else
-        hash[name] = 1
       end
     end
-  end
   hash
 end
 
